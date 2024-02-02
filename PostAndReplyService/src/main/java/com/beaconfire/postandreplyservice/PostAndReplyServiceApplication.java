040c4abe39001299c14c2ae791866381ba81aa70
pengc <peng.cheng1@northeastern.edu>
2024-02-02 18:59:32 -0500

    initial setup

---
+++ PostAndReplyService/src/main/java/com/beaconfire/postandreplyservice/PostAndReplyServiceApplication.java
@@ -0,0 +1,15 @@
+package com.beaconfire.postandreplyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PostAndReplyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostAndReplyServiceApplication.class, args);
+    }
+
+}

162f37dac234db4470f6d8826a49892d7deccbce
pengc <peng.cheng1@northeastern.edu>
2024-02-03 18:44:56 -0500

    history-serivice-initial
    Took 5 seconds

--- HistoryService/src/main/java/com/beaconfire/historyservice/service/HistoryService.java
+++ HistoryService/src/main/java/com/beaconfire/historyservice/service/HistoryService.java
@@ -1,31 +1,6 @@
 package com.beaconfire.historyservice.service;
 
-import com.beaconfire.historyservice.domain.History;
-import com.beaconfire.historyservice.repository.HistoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
 public class HistoryService {
-
-    private final HistoryRepository historyRepository;
-
-    @Autowired
-    public HistoryService(HistoryRepository historyRepository) {
-        this.historyRepository = historyRepository;
-    }
-
-    public List<History> findMostRecentHistoriesByUserId(Long userId) {
-        return historyRepository.findByUserIdOrderByViewDateDesc(userId);
-    }
-
-    public List<History> findAllHistoriesByUserId(Long userId) {
-        return historyRepository.findByUserId(userId);
-    }
-
-
 
     //get posts by userid, check post status, delete (?) non-"published" records
 

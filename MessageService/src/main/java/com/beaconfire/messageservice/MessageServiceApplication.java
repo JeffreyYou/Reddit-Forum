45e44d988960f0d25311b75f51a6635234e6c806
Charles Zhang <158590641+charlesZSW@users.noreply.github.com>
2024-02-03 12:11:08 -0600

    Gateway service dev orchestration (#5)
    Merge all microservice initial package/module into backend-dev for team to start with
    Tested Gateway and Eureka to ensure proper routing through a dummy endpoint created under user service
    
    Took 27 seconds

--- MessageService/src/main/java/com/beaconfire/messageservice/MessageServiceApplication.java
+++ MessageService/src/main/java/com/beaconfire/messageservice/MessageServiceApplication.java
@@ -3,11 +3,9 @@ package com.beaconfire.messageservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MessageServiceApplication {
 
     public static void main(String[] args) {

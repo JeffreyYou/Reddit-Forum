11599351f83b4f865b8606cc819e58cee5a63a25
charlesZSW <zhangsiweiv@outlook.com>
2024-02-04 12:34:23 -0600

    Added responses DTOs to APIs and implement user register API
    Took 1 hour 11 minutes
    
    Took 9 minutes
    
    Took 10 seconds

--- UserService/src/main/java/com/beaconfire/userservice/dto/UserAuthRequest/ChangePasswordRequest.java
+++ UserService/src/main/java/com/beaconfire/userservice/dto/UserAuthRequest/ChangePasswordRequest.java
@@ -13,7 +13,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @AllArgsConstructor
 public class ChangePasswordRequest {
 
+    @NotBlank(message = "Old password cannot be blank")
+    @Schema(description = "User's current password", example = "currentPassword123", required = true)
+    private String oldPassword;
+
     @NotBlank(message = "New password cannot be blank")
-    @Schema(description = "User's new password", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...", required = true)
+    @Schema(description = "User's new password", example = "newSecurePassword456", required = true)
     private String newPassword;
 }
---
+++ UserService/src/main/java/com/beaconfire/userservice/dto/UserAuthResponse/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package com.beaconfire.userservice.dto.UserAuthResponse;
+
+import lombok.Builder;
+import lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Data
+@Builder
+public class AuthenticationResponse {
+
+    @Schema(description = "Indicates if authentication was successful", example = "true")
+    private boolean authenticated;
+
+    @Schema(description = "User's hashed password", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
+    private String password;
+
+    @Schema(description = "Authentication confirmation or error message", example = "Authentication successful.")
+    private String message;
+}

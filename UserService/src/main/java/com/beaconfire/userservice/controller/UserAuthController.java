f29d32e2f5b5de910bfae6eb1acaf889bba5fe6d
charlesZSW <zhangsiweiv@outlook.com>
2024-02-03 18:51:25 -0600

    Added Auth Controller Request DTOs and Database JPA configuration
    Took 28 minutes
    
    
    Took 10 seconds

--- UserService/src/main/java/com/beaconfire/userservice/controller/UserAuthController.java
+++ UserService/src/main/java/com/beaconfire/userservice/controller/UserAuthController.java
@@ -1,10 +1,15 @@
 package com.beaconfire.userservice.controller;
 
 import com.beaconfire.userservice.domain.User;
+import com.beaconfire.userservice.dto.ChangePasswordRequest;
+import com.beaconfire.userservice.dto.EmailVerificationConfirmationRequest;
+import com.beaconfire.userservice.dto.UserAuthenticationRequest;
+import com.beaconfire.userservice.dto.UserRegistrationRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserAuthController {
 
+    // ResponseEntity<String> added for testing purposes, will be removed later
     @PostMapping("/user/register")
     @Operation(summary = "Registers a new user in the system",
             responses = {
@@ -19,7 +25,11 @@ public class UserAuthController {
                             content = @Content(schema = @Schema(implementation = User.class))),
                     @ApiResponse(responseCode = "400", description = "Invalid user registration details")
             })
-    public void registerUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+    public ResponseEntity<String> registerUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+        String username = userRegistrationRequest.getUsername();
+        String email = userRegistrationRequest.getEmail();
+        String password = userRegistrationRequest.getPassword();
+        return ResponseEntity.ok("User " + username + "\n with email " + email + "\nand password " + password + "\nhas been registered");
     }
 
     @PostMapping("/user/authenticate")
@@ -49,7 +59,4 @@ public class UserAuthController {
             })
     public void confirmEmailVerification(@RequestBody EmailVerificationConfirmationRequest emailVerificationConfirmationRequest) {
     }
-
-    // Define UserRegistrationRequest, UserAuthenticationRequest, ChangePasswordRequest,
-    // EmailVerificationInitiationRequest, EmailVerificationConfirmationRequest, and User classes accordingly
 }
---
+++ UserService/src/main/java/com/beaconfire/userservice/dto/UserAuthenticationRequest.java
@@ -0,0 +1,23 @@
+package com.beaconfire.userservice.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import javax.validation.constraints.NotBlank;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAuthenticationRequest {
+
+    @NotBlank(message = "Username cannot be blank")
+    @Schema(description = "User's username for login", example = "john_doe", required = true)
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Schema(description = "User's password for login", example = "securePassword123", required = true)
+    private String password;
+}

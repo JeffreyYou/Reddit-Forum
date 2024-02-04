fd944fa2fce1a560feaaaacf8feb17e04fe92476
JeffreyYou <yqse521749@gmail.com>
2024-02-04 00:04:58 -0800

    Spring Data JPA Get most recent posts by user ID Get all posts by user ID Swagger description
    Took 4 seconds

--- HistoryService/src/main/java/com/beaconfire/historyservice/config/SwaggerConfig.java
+++ HistoryService/src/main/java/com/beaconfire/historyservice/config/SwaggerConfig.java
@@ -7,13 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    /*@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();*/
 
     @Bean
     public GroupedOpenApi publicApi(){

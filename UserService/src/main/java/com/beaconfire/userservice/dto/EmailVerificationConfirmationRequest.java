969a7e1019cf09714ad6ff6afc4f04c7752111d2
charlesZSW <zhangsiweiv@outlook.com>
2024-02-03 18:51:25 -0600

    Added Auth Controller Request DTOs and Database JPA configuration
    Took 28 minutes
    
    Took 10 seconds
    
    Took 30 seconds

---
+++ UserService/src/main/java/com/beaconfire/userservice/dto/EmailVerificationConfirmationRequest.java
@@ -0,0 +1,25 @@
+package com.beaconfire.userservice.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailVerificationConfirmationRequest {
+
+    @NotNull(message = "User ID cannot be null")
+    @Schema(description = "The ID of the user", example = "1", required = true)
+    private Long userId;
+
+    @Email(message = "Invalid email format")
+    @NotNull(message = "Email cannot be null")
+    @Schema(description = "User's email address to be verified", example = "user@example.com", required = true)
+    private String email;
+
+    // Getters and Setters are provided by the @Data annotation from Lombok
+}

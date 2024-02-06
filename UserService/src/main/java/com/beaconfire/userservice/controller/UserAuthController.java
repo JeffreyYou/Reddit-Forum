011342a94dd1a2935241e4f27c6bd8f03b9d802d
charlesZSW <zhangsiweiv@outlook.com>
2024-02-06 12:40:42 -0600

    Refactored User Controllers/APIs
    Took 4 minutes

--- UserService/src/main/java/com/beaconfire/userservice/controller/UserAuthController.java
+++ UserService/src/main/java/com/beaconfire/userservice/controller/UserAuthController.java
@@ -8,6 +8,7 @@ import com.beaconfire.userservice.dto.UserAuthResponse.*;
 import com.beaconfire.userservice.service.EmailService;
 import com.beaconfire.userservice.service.UserAuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;

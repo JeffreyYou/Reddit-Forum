511362ba3df84ca2fbecb33891b519ae24fea079
kkty39 <yingyitong39@gmail.com>
2024-02-06 13:21:19 -0800

    added Swagger annotation

--- MessageService/src/main/java/com/beaconfire/messageservice/dto/msgmgmt/MessageResponse.java
+++ MessageService/src/main/java/com/beaconfire/messageservice/dto/msgmgmt/MessageResponse.java
@@ -1,6 +1,6 @@
 package com.beaconfire.messageservice.dto.msgmgmt;
 
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,10 +8,17 @@ import java.sql.Timestamp;
 
 @Data
 @Builder
+@Schema(description = "Response object for a message")
 public class MessageResponse {
+    @Schema(description = "Date created")
     private Timestamp dateCreated;
+
+    @Schema(description = "Email address")
     private String email;
+
+    @Schema(description = "Message content")
     private String message;
+
+    @Schema(description = "Message status")
     private String status;
 }
-

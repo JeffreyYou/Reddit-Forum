1b1e24c503314deae59b2aad93acb638ec5fba97
zzhou37 <zzhou37@ucsc.edu>
2024-02-05 18:49:54 -0800

    test git
    Took 5 seconds

--- UserService/src/main/java/com/beaconfire/userservice/config/SecurityConfig.java
+++ UserService/src/main/java/com/beaconfire/userservice/config/SecurityConfig.java
@@ -1,45 +1,18 @@
 package com.beaconfire.userservice.config;
 
-import com.beaconfire.userservice.security.JwtFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private JwtFilter jwtTokenFilter; // Your custom JWT filter
-
+    // This method is used to configure the security of the application
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
-                .csrf().disable() // Disable CSRF protection
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and() // Use stateless sessions
-                .authorizeRequests()
-                .antMatchers("/user-service/user/authenticate", "/user-service/user/create").permitAll()
-                .antMatchers("/user-service/swagger-ui/**", "/user-service/v3/**", "/user-service/swagger-ui*").permitAll()
-                .antMatchers("/user-service/**").permitAll()
-                .anyRequest().authenticated() // Require authentication for any other request
-                .and()
-                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class); // Add your custom JWT filter before the Spring Security filter
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Define the password encoder
+        http.csrf().disable();
+        // Additional HTTP security configurations go here
+        // zhihang common
     }
 }

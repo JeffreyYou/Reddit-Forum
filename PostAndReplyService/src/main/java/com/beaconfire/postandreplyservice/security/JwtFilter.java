0eb6149c39fbf678da467ee635b12e679986f0c8
Tianhao Gu <gtianhao@umich.edu>
2024-02-06 13:12:05 -0800

    All Post and Reply code (except feignclient), but mongo configuration in application.yml fail
    Took 5 seconds

--- PostAndReplyService/src/main/java/com/beaconfire/postandreplyservice/security/JwtFilter.java
+++ PostAndReplyService/src/main/java/com/beaconfire/postandreplyservice/security/JwtFilter.java
@@ -1,6 +1,5 @@
 package com.beaconfire.postandreplyservice.security;
 
-import com.beaconfire.postandreplyservice.util.JwtTokenHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,21 +21,14 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authorizationHeader = request.getHeader("Authorization");
-
-        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+        if (request.getHeader("Authorization") == null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write("Unauthorized User");
-            return; // Exit the filter chain early if there's no valid Authorization header
+            return;
         }
-
-        // At this point, we know the Authorization header is present and well-formed
-        String jwtToken = authorizationHeader.substring(7);
-        JwtTokenHolder.setToken(jwtToken);
-
         Optional<AuthUserDetail> authUserDetailOptional = jwtProvider.resolveToken(request); // extract jwt from request, generate a userdetails object
 
-        if (authUserDetailOptional.isPresent()) {
+        if (authUserDetailOptional.isPresent()){
             AuthUserDetail authUserDetail = authUserDetailOptional.get();
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                     authUserDetail.getUsername(),
@@ -44,14 +36,9 @@ public class JwtFilter extends OncePerRequestFilter {
                     authUserDetail.getAuthorities()
             ); // generate authentication object
 
-            SecurityContextHolder.getContext().setAuthentication(authentication); // put authentication object in the security context
+            SecurityContextHolder.getContext().setAuthentication(authentication); // put authentication object in the secruitycontext
         }
 
-        try {
-            filterChain.doFilter(request, response);
-        } finally {
-            // Ensure that the JWT token is cleared after the request has been processed
-            JwtTokenHolder.clear();
-        }
+        filterChain.doFilter(request, response);
     }
 }

379bf3226765909ca398432b44de7fce30765d79
kkty39 <yingyitong39@gmail.com>
2024-02-06 12:50:05 -0800

    change update status method, add getbyId method, fix Exception bug
    Took 8 seconds

--- MessageService/src/main/java/com/beaconfire/messageservice/dto/msgmgmt/MessageListResponse.java
+++ MessageService/src/main/java/com/beaconfire/messageservice/dto/msgmgmt/MessageListResponse.java
@@ -1,6 +1,5 @@
 package com.beaconfire.messageservice.dto.msgmgmt;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,11 +7,8 @@ import java.util.List;
 
 @Data
 @Builder
-@Schema(description = "Response object for a list of messages")
 public class MessageListResponse {
-    @Schema(description = "List of messages")
     private List<MessageResponse> messagesList;
-
-    @Schema(description = "Additional message or details about the response")
     private String message;
 }
+

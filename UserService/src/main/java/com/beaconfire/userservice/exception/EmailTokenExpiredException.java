093544bd3e0418f2e0ed8cc4df3c6ec735503226
Jeffrey You <52956161+JeffreyYou@users.noreply.github.com>
2024-02-07 11:44:14 -0800

    Revert "email service integrated"

--- UserService/src/main/java/com/beaconfire/userservice/exception/EmailTokenExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.beaconfire.userservice.exception;
-
-public class EmailTokenExpiredException extends RuntimeException {
-    public EmailTokenExpiredException(String message) {
-        super(message);
-    }
-}

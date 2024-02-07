06106e1b331b0134c45bbfcf2d3610effedff869
Jeffrey You <52956161+JeffreyYou@users.noreply.github.com>
2024-02-07 11:44:14 -0800

    Revert "email service integrated"
    Took 5 seconds

--- UserService/src/main/java/com/beaconfire/userservice/exception/EmailTokenNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.beaconfire.userservice.exception;
-
-public class EmailTokenNotFoundException extends RuntimeException {
-
-    public EmailTokenNotFoundException(String message) {
-        super(message);
-    }
-}

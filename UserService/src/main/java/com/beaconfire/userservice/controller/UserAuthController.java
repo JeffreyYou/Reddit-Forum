58be146a4c1e33fd736e3175a41bf1bc027abc3d
Jeffrey You <52956161+JeffreyYou@users.noreply.github.com>
2024-02-07 11:44:14 -0800

    Revert "email service integrated"
    Took 5 seconds

--- UserService/src/main/java/com/beaconfire/userservice/controller/UserAuthController.java
+++ UserService/src/main/java/com/beaconfire/userservice/controller/UserAuthController.java
@@ -3,15 +3,14 @@ package com.beaconfire.userservice.controller;
 import com.beaconfire.userservice.domain.User;
 import com.beaconfire.userservice.dto.ErrorDetails;
 import com.beaconfire.userservice.dto.UserAuthRequest.ChangePasswordRequest;
-import com.beaconfire.userservice.dto.UserAuthRequest.EmailVerificationRequest;
+import com.beaconfire.userservice.dto.UserAuthRequest.EmailVerificationConfirmationRequest;
 import com.beaconfire.userservice.dto.UserAuthRequest.UserAuthenticationRequest;
 import com.beaconfire.userservice.dto.UserAuthRequest.UserCreateRequest;
-import com.beaconfire.userservice.dto.UserAuthResponse.EmailVerificationResponse;
 import com.beaconfire.userservice.dto.UserAuthResponse.UserAuthenticationResponse;
 import com.beaconfire.userservice.dto.UserAuthResponse.ChangePasswordResponse;
+import com.beaconfire.userservice.dto.UserAuthResponse.EmailVerificationResponse;
 import com.beaconfire.userservice.dto.UserAuthResponse.UserCreateResponse;
 
-import com.beaconfire.userservice.service.EmailService;
 import com.beaconfire.userservice.service.UserAuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -23,19 +22,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @RestController
 @CrossOrigin(origins = "*")
 public class UserAuthController {
 
     private final UserAuthService userAuthService;
-    private final EmailService emailService;
 
     @Autowired
-    public UserAuthController(UserAuthService userAuthService, EmailService emailService) {
+    public UserAuthController(UserAuthService userAuthService) {
         this.userAuthService = userAuthService;
-        this.emailService = emailService;
     }
 
     @PostMapping("/user/create")
@@ -49,17 +44,7 @@ public class UserAuthController {
     public ResponseEntity<UserCreateResponse> createUser(@RequestBody UserCreateRequest userCreateRequest) {
         final String email = userCreateRequest.getEmail();
         final String password = userCreateRequest.getPassword();
-//        final User newUser = userAuthService.createUser(email, password);
-
-        // send verification email
-        // get first name
-        final String firstname = userCreateRequest.getFirstname();
-        final String lastname = userCreateRequest.getLastname();
-        final String emailToken = emailService.sendEmail(email, firstname);
-
-        // save token to the database with an expired time (bonus)
-        final User newUser = userAuthService.createUser(email, password, firstname, lastname, emailToken);
-
+        final User newUser = userAuthService.createUser(email, password);
 
         return ResponseEntity.ok(UserCreateResponse.builder()
                 .success(true)
@@ -104,24 +89,5 @@ public class UserAuthController {
                 .success(true)
                 .message("Password changed successfully.")
                 .build());
-    }
-
-    /*****   email verification (newly added)  ********/
-    @PostMapping("/user/verify")
-    @PreAuthorize("hasAnyAuthority('ROLE_SADMIN')")
-    @Operation(summary = "Allows users to verify their email",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Email verified successfully",
-                            content = @Content(schema = @Schema(implementation = EmailVerificationResponse.class))),
-                    @ApiResponse(responseCode = "400", description = "Invalid email verify request",
-                            content = @Content(schema = @Schema(implementation = ErrorDetails.class))),
-            })
-    public ResponseEntity<EmailVerificationResponse> verifyEmail(@Valid @RequestBody EmailVerificationRequest emailVerificationRequest) {
-
-        return ResponseEntity.ok(EmailVerificationResponse.builder()
-                .verified(emailService.verifyEmail(emailVerificationRequest.getToken()))
-                .message("Email verified successfully!")
-                .build());
-
     }
 }

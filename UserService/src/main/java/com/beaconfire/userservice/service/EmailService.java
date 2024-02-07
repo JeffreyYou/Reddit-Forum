ce8dc830f07066bc9c856f6bba65bd35743824db
jinweibi <bijinwei@seas.upenn.edu>
2024-02-07 03:04:53 -0500

    validation add, and handle email token expired exception.
    Took 10 seconds

--- UserService/src/main/java/com/beaconfire/userservice/service/EmailService.java
+++ UserService/src/main/java/com/beaconfire/userservice/service/EmailService.java
@@ -3,7 +3,8 @@ package com.beaconfire.userservice.service;
 import com.beaconfire.userservice.dao.UserRepository;
 import com.beaconfire.userservice.domain.User;
 import com.beaconfire.userservice.domain.message.EmailMessage;
-import com.beaconfire.userservice.exception.*;
+import com.beaconfire.userservice.exception.EmailTokenExpiredException;
+import com.beaconfire.userservice.exception.EmailTokenNotFoundException;
 import com.beaconfire.userservice.util.EmailMessageUtil;
 import com.beaconfire.userservice.util.SerializeUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -20,19 +22,13 @@ public class EmailService {
     private final RabbitTemplate rabbitTemplate;
     private final UserRepository userRepository;
 
-    private static final long emailValidPeriod = 3 * 60 * 60 * 1000; // 3 hrs
-
     private final String emailExchange = "emailExchange";
     private final String emailRoutingKey = "emailRoutingKey";
 
     @Autowired
-    public EmailService(RabbitTemplate rabbitTemplate, UserRepository userRepository) {
+    public EmailService(RabbitTemplate rabbitTemplate, ObjectMapper objectMapper, UserRepository userRepository) {
         this.rabbitTemplate = rabbitTemplate;
         this.userRepository = userRepository;
-    }
-
-    public static long getEmailValidPeriod() {
-        return emailValidPeriod;
     }
 
     public String sendEmail(String email, String firstName) {
@@ -72,27 +68,5 @@ public class EmailService {
         } else {
             throw new EmailTokenExpiredException("The verification token has expired! Please request once again.");
         }
-    }
-
-    public boolean updateUserEmail(Long userId, String newEmail) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User with id " + userId + " not found."));
-        System.out.println(user.getEmail());
-        if (user.getEmail().equals(newEmail) && user.isVerified())
-            throw new EmailVerifiedAndUnchangedException("Email is already verified and unchanged.");
-        else if (user.getEmail().equals(newEmail))
-            throw new EmailVerifiedAndUnchangedException("Email already in use. Please verify your email!");
-        // check if the new email already exists and used by another user
-        if (userRepository.findByEmail(newEmail).isPresent()) {
-            throw new UserAlreadyExistsException("User with email " + newEmail + " already exists.");
-        }
-        // update email and send verification link
-        user.setVerified(false);
-        user.setEmail(newEmail);
-        String token = sendEmail(newEmail, user.getFirstName());
-        user.setEmailToken(token);
-        user.setEmailTokenExpiredTime(new Timestamp(System.currentTimeMillis() + emailValidPeriod));
-        userRepository.save(user);
-
-        return true;
     }
 }

5deaa385f188383c46f612be2b1f4ec795dd8bb3
jinweibi <bijinwei@seas.upenn.edu>
2024-02-08 01:50:42 -0800

    login/register/user-maanagement update

--- reddit-frontend/src/pages/login/LoginPage.tsx
+++ reddit-frontend/src/pages/login/LoginPage.tsx
@@ -1,118 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { Modal, Form, Input, Button, Divider } from "antd";
-import { GoogleOutlined, AppleOutlined } from "@ant-design/icons";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import styles from "./style.module.scss";
+import LoginForm, { ILoginFormValues } from "../../components/forms/login-form";
+import useAuthStore, { ILoginResponse } from "../../store/auth-store";
+import { message } from "antd";
 
-const LoginPage = () => {
-  //   const [isModalVisible, setIsModalVisible] = useState(true); // You can control the visibility as needed
+const LoginPage: React.FC = () => {
+  // Access the signIn function from the authStore
+  const signIn = useAuthStore((state) => state.signIn);
 
-  const handleSubmit = (values) => {
+  const handleSubmit = async (values: ILoginFormValues) => {
     console.log("Received values of form: ", values);
-    // Place your login logic here
-    console.log(values);
+    try {
+      // Assuming your ILoginFormValues has email and password
+      await signIn(values.email, values.password);
+      console.log("Login successful");
+
+      // Redirect to homepage or dashboard after successful login
+      // This could be a React Router redirect or a window.location change, depending on your routing setup
+      // Example: history.push('/dashboard');
+    } catch (error) {
+      console.error("Login failed", error);
+      // Handle login failure, such as showing an error message
+      message.error(error.message);
+    }
   };
 
-  const navigate = useNavigate();
-
-  const handleRegisterClick = () => {
-    navigate("/users/register");
-  };
-
-  //   return (
-  //     <Modal
-  //       title="Log In"
-  //       centered
-  //       open={isModalVisible}
-  //       footer={null} // No default footer
-  //       onCancel={() => setIsModalVisible(false)}
-  //     >
-  //       <Form
-  //         name="login_form"
-  //         initialValues={{ remember: true }}
-  //         onFinish={handleSubmit}
-  //       >
-  //         <Form.Item
-  //           name="username"
-  //           rules={[{ required: true, message: "Please input your Username!" }]}
-  //         >
-  //           <Input placeholder="Username" />
-  //         </Form.Item>
-  //         <Form.Item
-  //           name="password"
-  //           rules={[{ required: true, message: "Please input your Password!" }]}
-  //         >
-  //           <Input.Password placeholder="Password" />
-  //         </Form.Item>
-
-  //         <Form.Item>
-  //           <Button type="primary" htmlType="submit" block>
-  //             Log In
-  //           </Button>
-  //         </Form.Item>
-
-  //         <Divider>OR</Divider>
-
-  //         <Form.Item>
-  //           <Button block icon={<GoogleOutlined />}>
-  //             Continue with Google
-  //           </Button>
-  //         </Form.Item>
-  //         <Form.Item>
-  //           <Button block icon={<AppleOutlined />}>
-  //             Continue with Apple
-  //           </Button>
-  //         </Form.Item>
-  //       </Form>
-  //       {/* <a href="#">Forgot your username or password?</a> */}
-  //       <br />
-  //       New to RedditHub?
-  //       <a onClick={handleRegisterClick}>Sign Up</a>
-  //     </Modal>
-  //   );
   return (
     <div className={styles.login_container}>
       <h2>Log In</h2>
-      <Form
-        name="login_form"
-        initialValues={{ remember: true }}
-        onFinish={handleSubmit}
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: "Please input your Username!" }]}
-        >
-          <Input placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input.Password placeholder="Password" />
-        </Form.Item>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit" block>
-            Log In
-          </Button>
-        </Form.Item>
-
-        <Divider>OR</Divider>
-
-        <Form.Item>
-          <Button block icon={<GoogleOutlined />}>
-            Continue with Google
-          </Button>
-        </Form.Item>
-        <Form.Item>
-          <Button block icon={<AppleOutlined />}>
-            Continue with Apple
-          </Button>
-        </Form.Item>
-      </Form>
-      <div className="additional-links">
-        New to RedditHub?
-        <a href="/users/register">Sign Up</a>
+      <LoginForm onSubmit={handleSubmit} />
+      <div className={styles.additionalLink}>
+        New to RedditHub? <a href="/users/register">Sign Up</a>
       </div>
     </div>
   );
